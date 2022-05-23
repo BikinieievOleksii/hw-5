5262817d43e8f2244371afa6ff775e8536bf7c68
Oleksii Bikinieiev <Tirexys@gmail.com>
2022-05-23 23:12:29 +0300

    modified LevelLoader

--- LevelLoaderTest.java
+++ LevelLoaderTest.java
@@ -2,15 +2,15 @@ class LevelLoaderTest {
     public static void main(String[] args) {
         //Level loaded
         try {
-            new ua.goit.module5.part6.LevelLoader().load(new ua.goit.module5.part6.Level(10, 20));
-        } catch (ua.goit.module5.part6.LevelTooBigException ex) {
+            new LevelLoader().load(new Level(10, 20));
+        } catch (LevelTooBigException ex) {
             System.out.println("Level too big");
         }
 
         //Level too big
         try {
-            new ua.goit.module5.part6.LevelLoader().load(new ua.goit.module5.part6.Level(10000, 2000));
-        } catch (ua.goit.module5.part6.LevelTooBigException ex) {
+            new LevelLoader().load(new Level(10000, 2000));
+        } catch (LevelTooBigException ex) {
             System.out.println("Level too big");
         }
     }
@@ -33,7 +33,7 @@ class Level {
         }
     }
 class LevelLoader {
-    public void load(ua.goit.module5.part6.Level level) throws LevelTooBigException {
+    public void load(Level level) throws LevelTooBigException {
             if (level.getHeight() * level.getWidth() > 10000) {
                 throw new LevelTooBigException();
             }
